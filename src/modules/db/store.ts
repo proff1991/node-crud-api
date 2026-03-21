27a64765962c8a6f97394a82314ac57d8f2adddf
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-03-21 23:59:42 +0200

    feat: add database in store.ts, install "zod", add product types

---
+++ src/modules/db/store.ts
@@ -0,0 +1,25 @@
+import { Product } from "../models/product.model";
+
+const products = new Map<string, Product>();
+
+export const productStore = {
+  getAll(): Product[] {
+    return Array.from(products.values());
+  },
+
+  getById(id: string): Product | undefined {
+    return products.get(id);
+  },
+
+  create(product: Product): void {
+    products.set(product.id, product);
+  },
+
+  update(id: string, product: Product): void {
+    products.set(id, product);
+  },
+
+  delete(id: string): boolean {
+    return products.delete(id);
+  },
+};

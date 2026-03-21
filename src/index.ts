0055e2736a666a806dd5ec9899bd36cd7a3ea6f8
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-03-21 22:47:12 +0200

    feat: add error catcher

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { getEnv } from "./config/env";
 
 (async () => {
-    console.log(getEnv().PORT);
+    try {
+        var PORT = getEnv().PORT;
+        console.log(PORT);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        } else {
+            console.error("Unknown error", error);
+        }
+        process.exit(1);
+    }
 })();
